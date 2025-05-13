3a887aae362771c4b73098df35ffef5f8c846ec9
Juan Diego Duque <juandiegoduquejimenez@gmail.com>
2025-05-12 20:12:30 -0500

    Commit #2: Se suben las demás dependencias del proyecto.

---
+++ src/main/java/com/example/InnoSistemas/InnoSistemasApplication.java
@@ -0,0 +1,13 @@
+package com.example.InnoSistemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnoSistemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnoSistemasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/InnoSistemas/InnoSistemasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InnoSistemas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnoSistemasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
