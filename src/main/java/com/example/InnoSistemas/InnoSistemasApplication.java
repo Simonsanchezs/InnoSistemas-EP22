2e30490edb5f5ca2d9d44b58df195c4e88ac645b
Simonsanchezs <simon.sanchezs@udea.edu.co>
2025-06-13 16:56:59 -0500

    cambios sonar

--- src/main/java/com/example/InnoSistemas/InnoSistemasApplication.java
+++ src/main/java/com/example/InnoSistemas/InnoSistemasApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.example.InnoSistemas.repository")
+@EnableJpaRepositories(basePackages = "com.example.innosistemas.repository")
 public class InnoSistemasApplication {
 
 	public static void main(String[] args) {
